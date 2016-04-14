7c73263d2460d99728392b44fe7f93a56989d07b
Delta-Dog <jonappel@outlook.com>
2016-04-14 02:44:25 +0000

    Bunch of Stuff

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,8 @@ resources :users, except: [:new]
 get 'login', to: 'sessions#new'
 post 'login', to: 'sessions#create'
 delete 'logout', to: 'sessions#destroy'
+
+resources :categories, except: [:destroy]
  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
